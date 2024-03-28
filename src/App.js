2a7c9ef3d5f33bb2daa6ba914da9eea4031c6daf
Pawan Kumar Singh <pawankumar.s@truglobal.com>
2024-03-28 16:41:47 +0530

    Dev branch changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-		  Hello World React by Pawan!!!        
+		  Hello World React by Pawan, Development Branch!!!        
         </p>
         <a
           className="App-link"
